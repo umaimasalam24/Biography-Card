9bcf32b4ae56d3e951bf7689170e31b5629c97fd
Umaima Salam <umaimasalam2003@gmail.com>
2025-08-24 14:38:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,114 @@
+let readmore = document.getElementById('readmore');
+let extrapara = document.getElementById('extrapara');
+readmore.onclick = (e) =>{
+    e.preventDefault();
+if(extrapara.style.display==='none'){
+    extrapara.style.display= 'inline';
+    readmore.textContent = 'Read Less'
+}
+else{
+   extrapara.style.display= 'none';
+    readmore.textContent = 'Read More' 
+}
+}
+
+let readMore = document.getElementById('readMore');
+let extraPara = document.getElementById('extraPara');
+readMore.onclick = (e) =>{
+        e.preventDefault();
+if(extraPara.style.display==='none'){
+    extraPara.style.display= 'inline';
+    readMore.textContent = 'Read Less'
+}
+else{
+   extraPara.style.display= 'none';
+    readMore.textContent = 'Read More' 
+}
+}
+
+let read = document.getElementById('read');
+let extra = document.getElementById('extra');
+read.onclick = (e) =>{
+        e.preventDefault();
+if(extra.style.display==='none'){
+    extra.style.display= 'inline';
+    read.textContent = 'Read Less'
+}
+else{
+   extra.style.display= 'none';
+    read.textContent = 'Read More' 
+}
+}
+
+
+let Read = document.getElementById('Read');
+let Extra = document.getElementById('Extra');
+Read.onclick = (e) =>{
+        e.preventDefault();
+if(Extra.style.display==='none'){
+    Extra.style.display= 'inline';
+    Read.textContent = 'Read Less'
+}
+else{
+   Extra.style.display= 'none';
+    Read.textContent = 'Read More' 
+}
+}
+
+let More = document.getElementById('More');
+let Para = document.getElementById('Para');
+More.onclick = (e) =>{
+        e.preventDefault();
+if(Para.style.display==='none'){
+    Para.style.display= 'inline';
+    More.textContent = 'Read Less'
+}
+else{
+   Para.style.display= 'none';
+    More.textContent = 'Read More' 
+}
+}
+
+let ReadM = document.getElementById('ReadM');
+let ExtraP = document.getElementById('ExtraP');
+ReadM.onclick = (e) =>{
+        e.preventDefault();
+if(ExtraP.style.display==='none'){
+    ExtraP.style.display= 'inline';
+    ReadM.textContent = 'Read Less'
+}
+else{
+   ExtraP.style.display= 'none';
+    ReadM.textContent = 'Read More' 
+}
+}
+
+let click = document.getElementById('click');
+let longpara = document.getElementById('longpara');
+click.onclick = (e) =>{
+        e.preventDefault();
+if(longpara.style.display==='none'){
+    longpara.style.display= 'inline';
+    click.textContent = 'Read Less'
+}
+else{
+  longpara.style.display= 'none';
+   click.textContent = 'Read More' 
+}
+}
+
+let Click = document.getElementById('Click');
+let Long = document.getElementById('Long');
+Click.onclick = (e) =>{
+        e.preventDefault();
+if(Long.style.display==='none'){
+    Long.style.display= 'inline';
+    Click.textContent = 'Read Less'
+}
+else{
+  Long.style.display= 'none';
+   Click.textContent = 'Read More' 
+}
+}
+
+
